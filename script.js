c9f4a729b74bf6de5d148ca67614c6b3235cd907
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-04-04 18:14:26 -0300

    #3 fix: generic algorithm working but slow

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var HEIGHT = window.innerHeight;
-var WIDTH = window.innerWidth;
-
 function createBg(left, top, width, height, tag, parent) {
   if (width == 0 || height == 0) {
     return;
@@ -13,9 +10,8 @@ function createBg(left, top, width, height, tag, parent) {
   bg.style.left = left + "px";
   bg.style.width = Math.abs(width) + "px";
   bg.style.height = Math.abs(height) + "px";
-  bg.setAttribute("class", tag);
-  bg.setAttribute("owner", parent);
   document.body.appendChild(bg);
+  return bg;
 }
 
 function randomFly(x, y) {
@@ -45,176 +41,94 @@ function collision(rect1, rect2) {
   );
 }
 
-function createArea(x, y, width, height) {
-  return { x, y, width, height };
-}
-
-function searchAndCreate(areas, cords, depth = 0) {
-  let collide = false;
-  loop1: for (let a in areas) {
-    const area = areas[a];
-    loop2: for (let c in cords) {
-      const cord = cords[c];
-      if (collision(area, cord)) {
-        collide = true;
-        if (area.y + area.height > cord.y) {
-          console.log(area);
-          area.height = area.y - cord.y;
-          console.log(area);
-        } else if (
-          area.x + area.width > cord.x &&
-          area.y < cord.y + cord.height &&
-          area.y + area.height > cord.y
-        ) {
-          console.log("hi");
-          area.width = cord.x;
-        } else if (
-          area.x < cord.x + cord.width &&
-          area.y < cord.y + cord.height &&
-          area.y + area.height > cord.y
-        ) {
-          area.width = cord.x;
-        }
-        areas.push(createArea(area.x, cord.y, cord.x, cord.height));
-        areas.push(
-          createArea(
-            cord.x + cord.width,
-            cord.y,
-            WIDTH - cord.x - cord.width,
-            cord.height
-          )
-        );
-        areas.push(
-          createArea(
-            area.x,
-            cord.y + cord.height,
-            WIDTH,
-            HEIGHT - cord.y - cord.height
-          )
-        );
-        break loop1;
+function loopX(cords, line, endX) {
+  let x;
+  for (x = line.x; x < endX; x++) {
+    const tempLine = { ...line };
+    tempLine.width = x - line.x;
+    for (let c in cords) {
+      if (collision(cords[c], tempLine)) {
+        x -= 1;
+        return { collided: true, x: x, cord: cords[c] };
       }
     }
   }
-  if (collide && depth < 1) {
-    return searchAndCreate(areas, cords, depth + 1);
+  return { collided: false, x: x, cord: null };
+}
+
+function searchAndCreate(cords, maxWidth, maxHeight) {
+  const baseLine = { x: 0, y: 0, width: 0, height: 0 };
+  const lines = [];
+  loopy: for (let y = 0; y < maxHeight; y++) {
+    const line = { ...baseLine };
+    line.y = y;
+    let result = {
+      collided: true,
+      x: 0,
+    };
+    while (result.collided) {
+      result = loopX(cords, line, maxWidth);
+      if (result.x - line.x > 0) {
+        lines.push({ x: line.x, y: y, width: result.x - line.x, height: 1 });
+      }
+      if (result.cord) {
+        line.x = result.cord.x + result.cord.width;
+        line.width = 1;
+      }
+    }
+  }
+
+  const areas = [];
+  for (let l = 0; l < lines.length; l++) {
+    let broke = false;
+    for (let a in areas) {
+      if (
+        lines[l].y - areas[a].lastY === 1 &&
+        lines[l].x === areas[a].x &&
+        lines[l].width === areas[a].width
+      ) {
+        areas[a].height++;
+        areas[a].lastY = lines[l].y;
+        broke = true;
+        break;
+      }
+    }
+    if (!broke) areas.push({ ...lines[l], lastY: lines[l].y });
   }
   return areas;
 }
 
-function searchBoxV2(cords, maxX, maxY) {
-  cords.sort((a, b) => (a.y > b.y ? 1 : -1)); //up to down order
-  const bigArea = {
-    x: 0,
-    y: 0,
-    width: WIDTH,
-    height: HEIGHT
-  };
-  const result = searchAndCreate([bigArea], cords);
-  for (let r in result) {
-    const box = result[r];
-    createBg(box.x, box.y, box.width, box.height);
+function generateBg(width = null, height = null) {
+  if (!width) {
+    width = window.innerWidth;
   }
-}
-
-function createBox(list, data) {
-  const object = {
-    ...data
-  };
-  object.id = list.length;
-  object.top = data.y;
-  object.left = data.x;
-  object.right = data.x + data.width;
-  object.bottom = data.y + data.height;
-  return object;
-}
-
-function generateBg() {
+  if (!height) {
+    height = window.innerHeight;
+  }
   var boxes = document.querySelectorAll(".fly");
-  var boxesCordsY = [];
+  var cords = [];
   for (var b = 0; b < boxes.length; b++) {
-    var o = boxes[b].getBoundingClientRect();
-    boxesCordsY.push(o);
+    cords.push(boxes[b].getBoundingClientRect());
   }
-  boxesCordsY.sort(function(a, b) {
+  cords.sort(function (a, b) {
     return a.y - b.y;
   });
-  console.log(boxesCordsY);
-  const cleanedBoxes = [];
-  const skipBoxes = [];
-  for (let b1 = 0; b1 < boxesCordsY.length; b1++) {
-    let didBreak = false;
-    for (let b2 = 0; b2 < boxesCordsY.length; b2++) {
-      if (
-        b1 !== b2 &&
-        skipBoxes.indexOf(b1 + "." + b2) === -1 &&
-        skipBoxes.indexOf(b2 + "." + b1) === -1
-      ) {
-        if (
-          boxesCordsY[b1].x < boxesCordsY[b2].x + boxesCordsY[b2].width &&
-          boxesCordsY[b1].x + boxesCordsY[b1].width > boxesCordsY[b2].x &&
-          boxesCordsY[b1].y < boxesCordsY[b2].y + boxesCordsY[b2].height &&
-          boxesCordsY[b1].height + boxesCordsY[b1].y > boxesCordsY[b2].y
-        ) {
-          const minX =
-            boxesCordsY[b1].x < boxesCordsY[b2].x
-              ? boxesCordsY[b1].x
-              : boxesCordsY[b2].x;
-          const minY =
-            boxesCordsY[b1].y < boxesCordsY[b2].y
-              ? boxesCordsY[b1].y
-              : boxesCordsY[b2].y;
-          const maxX =
-            boxesCordsY[b1].x + boxesCordsY[b1].width >
-            boxesCordsY[b2].x + boxesCordsY[b2].width
-              ? boxesCordsY[b1].x + boxesCordsY[b1].width
-              : boxesCordsY[b2].x + boxesCordsY[b2].width;
-          console.log(maxX);
-          const maxY =
-            boxesCordsY[b1].y + boxesCordsY[b1].height >
-            boxesCordsY[b2].y + boxesCordsY[b2].height
-              ? boxesCordsY[b1].y + boxesCordsY[b1].height
-              : boxesCordsY[b2].y + boxesCordsY[b2].height;
-
-          cleanedBoxes.push(
-            createBox(cleanedBoxes, {
-              x: minX,
-              y: minY,
-              width: maxX - minX,
-              height: maxY - minY
-            })
-          );
-          skipBoxes.push(b1 + "." + b2);
-          didBreak = true;
-          break;
-        }
-      }
-      if (
-        skipBoxes.indexOf(b1 + "." + b2) !== -1 ||
-        skipBoxes.indexOf(b2 + "." + b1) !== -1
-      ) {
-        didBreak = true;
-        break;
-      }
-    }
-    if (!didBreak) {
-      cleanedBoxes.push(boxesCordsY[b1]);
-    }
+  const result = searchAndCreate(cords, width, height);
+  for (let r in result) {
+    createBg(result[r].x, result[r].y, result[r].width, result[r].height);
   }
-  for (var b = 0; b < cleanedBoxes.length; b++) {
-    cleanedBoxes[b].id = b + 1;
-  }
-  searchBoxV2(cleanedBoxes, WIDTH, HEIGHT);
 }
 
-window.onload = function() {
+window.onload = function () {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
   if (document.querySelectorAll(".fly").length === 0) {
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 10; i++) {
       randomFly(
-        Math.floor(Math.random() * (WIDTH - 100) + 1),
-        Math.floor(Math.random() * (HEIGHT - 100) + 1)
+        Math.floor(Math.random() * (width - 100) + 1),
+        Math.floor(Math.random() * (height - 100) + 1)
       );
     }
   }
-  generateBg();
+  generateBg(width, height);
 };
